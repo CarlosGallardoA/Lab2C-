a5a5014d763d182dbcd8275b1fddc171ffd38e70
Carlos Gallardo <carlitosgallardo009@gmail.com>
2022-03-30 12:24:43 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la coordenada en x del punto 1: ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la coordenada en y del punto 1: ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la coordenada en x del punto 2: ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la coordenada en y del punto 2: ");
+            int y2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la coordenada en x del punto 3: ");
+            int x3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la coordenada en y del punto 3: ");
+            int y3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la coordenada en x del punto 4: ");
+            int x4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la coordenada en y del punto 4: ");
+            int y4 = int.Parse(Console.ReadLine());
+            Rectangulo rectangulo = new Rectangulo();
+            
+            int area = rectangulo.CalcularArea(x1,y1,x2,y2,x3,y3,x4,y4);
+            int perimetro = rectangulo.CalcularPerimetro(x1,y1,x2,y2,x3,y3,x4,y4);
+
+            Console.WriteLine("El area es " + area);
+            Console.WriteLine("El perimetro es " + perimetro);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Rectangulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Rectangulo
+    {
+        public punto p1 { get; set; }
+        public punto p2 { get; set; }
+        public punto p3 { get; set; }
+        public punto p4 { get; set; }
+       
+        public int CalcularArea(int x1,int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+        {
+            int area = Math.Abs((x1 * y2) + (x2 * y3) + (x3 * y4) - ((x2 * y1) + (x3 * y2) + (x4 * y3)));
+
+            return area / 2;
+        }
+        public int CalcularPerimetro(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+        {
+            int perimetro = (int)Math.Sqrt(Math.Pow((x4 - x1), 2) + Math.Pow((y4 - y1),2)) + (int)Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2)) + (int)Math.Sqrt(Math.Pow((x2 - x3), 2) + Math.Pow((y2 - y3), 2)) + (int)Math.Sqrt(Math.Pow((x1 - x4), 2) + Math.Pow((y1 - y4), 2));
+            return perimetro;
+        }
+    }
+}
